6bcfd9bcae4b32e45cb312ba1cef12b41e641ccb
MOSCOW\Andrei_Sterkhov <andrei_sterkhov@epam.com>
2018-07-18 15:39:16 +0400

    another commit

--- src/main/java/com/epam/andrei_sterkhov/online_shop/repository/CategoryRepository.java
+++ src/main/java/com/epam/andrei_sterkhov/online_shop/repository/CategoryRepository.java
@@ -1,7 +1,11 @@
 package com.epam.andrei_sterkhov.online_shop.repository;
 
+import com.epam.andrei_sterkhov.online_shop.dto.Category;
+
 public interface CategoryRepository {
     void createCategory(String name);
 
     void deleteCategory(String name);
+
+    Category getCategoryByName(String name);
 }
--- src/main/java/com/epam/andrei_sterkhov/online_shop/repository/impl/CategoryRepositoryImpl.java
+++ src/main/java/com/epam/andrei_sterkhov/online_shop/repository/impl/CategoryRepositoryImpl.java
@@ -2,10 +2,12 @@ package com.epam.andrei_sterkhov.online_shop.repository.impl;
 
 import com.epam.andrei_sterkhov.online_shop.dto.Category;
 import com.epam.andrei_sterkhov.online_shop.repository.CategoryRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Repository
 public class CategoryRepositoryImpl implements CategoryRepository {
     private Map<String, Category> categories = new HashMap<>();
 
@@ -19,5 +21,10 @@ public class CategoryRepositoryImpl implements CategoryRepository {
     @Override
     public void deleteCategory(String name) {
         categories.remove(name);
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categories.get(name);
     }
 }
